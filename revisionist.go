93af402fb13ad5b26673eca7c39aac5cf6dcc700
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2020-01-25 09:21:13 -0500

    Update revisionist.go

--- revisionist.go
+++ revisionist.go
@@ -1,15 +1,58 @@
+-bash-4.3$ cat *.go
 package main
 
 import (
     "bufio"
     "fmt"
+    "io/ioutil"
+    "os/user"
     "os"
     "regexp"
     "strings"
 )
 
+func append_netrc(machine string, login string, password string)() {
+        usr,_ := user.Current()
+        netrc_file := string(usr.HomeDir) + "/.netrc"
+
+        // *IF* there is no ~/.netrc, create one
+        _,err := os.Open(netrc_file)
+
+        if (err != nil) {
+                f,_ := os.Create(netrc_file)
+                f.WriteString("#")
+                f.Close()
+        } else {
+                netrc_data,_ := ioutil.ReadFile(netrc_file)
+
+                if !strings.Contains(string(netrc_data), machine) {
+                        netrc_entry := "\nmachine\t" + machine + "\nlogin\t" + login + "\npassword\t" + password + "\n\n"
+
+                        f,_ := os.OpenFile(netrc_file, os.O_APPEND|os.O_WRONLY, 0600)
+                        f.WriteString(netrc_entry)
+                        f.Close()
+                }
+        }
+}
+
 func curl_u(cmdline string)(after string) {
     curl := regexp.MustCompile(`:\S+?\s+\b`)
+
+    token := strings.Trim(curl.FindString(cmdline), ":")
+
+    if len(token) > 0 {
+        // If a valid token is found, try and extract username and host
+        // and append to ~/.netrc
+
+        user_regex := regexp.MustCompile(`\S+?:\b`)
+        user := strings.Trim(user_regex.FindString(cmdline), ":")
+
+        url_regex := regexp.MustCompile(`:\/\/(\S+\.\w*)`)
+        url := strings.Trim(url_regex.FindString(cmdline), ":/")
+
+        append_netrc(url, user, token)
+    }
+
     after = curl.ReplaceAllString(cmdline, ":REDACTED ")
     return
 }
@@ -46,12 +89,13 @@ func main() {
         // Redact credential patterns
         //
 
-	text = newtext
+        text = newtext
 
         text = curl_u(text)
-	text = https_creds(text)
-	text = header_creds(text)
+        text = https_creds(text)
+        text = header_creds(text)
 
         fmt.Println(text)
     }
 }
+-bash-4.3$
